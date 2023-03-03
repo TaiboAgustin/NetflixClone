0179ac5f1a744091e974b48059d313bd2f10d511
Agustin Taibo <taibocruz@gmail.com>
2023-03-02 21:55:42 -0300

    starting project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBv0-HvuYiSOUZ5NoJvVI4CmofeAIn_p14",
+    authDomain: "netflix-clone-9bde1.firebaseapp.com",
+    projectId: "netflix-clone-9bde1",
+    storageBucket: "netflix-clone-9bde1.appspot.com",
+    messagingSenderId: "515433267789",
+    appId: "1:515433267789:web:74106d26f8de775b2a5204"
+  };
